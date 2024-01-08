efe267f3f9f0c7077c97aa98a74715234f160712
Emmanuel Banahene Asante <emmaasante676@gmail.com>
2024-01-08 19:00:58 +0000

    implement functions for appending number and decimal point

--- script.js
+++ script.js
@@ -4,40 +4,48 @@ let firstOperand = "";
 let secondOperand = "";
 
 const numberButtons = document.querySelectorAll(".number");
-
 const operators = document.querySelectorAll(".operator");
-
 const clearButton = document.querySelector(".clear-button");
-
 const deleteButton = document.querySelector(".delete-button");
-
 const displayScreen = document.querySelector(".display-screen");
-
 const equalsButton = document.querySelector(".equals");
-
 const pointButton = document.querySelector(".point");
+
+const maxCharacters = 14;
+
+// Event listeners for all events
 
 clearButton.addEventListener("click", clearScreen);
 
+deleteButton.addEventListener("click", deleteCharacter);
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", appendNumber);
+});
+
+pointButton.addEventListener("click", appendDecimal);
+
+function appendDecimal() {
+  console.log("clicked", displayScreen.textContent);
+  if (displayScreen.textContent === "") displayScreen.textContent = "0";
+  if (displayScreen.textContent.includes(".")) return;
+  displayScreen.textContent += ".";
+}
+
+function appendNumber() {
+  const currentContent = displayScreen.textContent;
+  if (currentContent.length < maxCharacters)
+    displayScreen.textContent += this.textContent;
+}
+
 function clearScreen() {
-  displayScreen.textContent = "0ghjg";
+  displayScreen.textContent = "0";
   firstOperand = "";
   secondOperand = "";
 }
 
-deleteButton.addEventListener("click", deleteCharacter);
-
 function deleteCharacter() {
-  let currentContent = displayScreen.textContent;
   let newContent = displayScreen.textContent.slice(0, -1);
 
   displayScreen.textContent = newContent;
 }
-
-// console.log(displayScreen.textContent.toString().slice(0, -3));
-
-// let name1 = "Frank";
-// // name1.slice(0, -1);
-// console.log(name1);
-// let num = 89;
-// console.log(num);
